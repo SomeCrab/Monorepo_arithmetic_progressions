05f6cf87aec097f9438f36de7739940276527591
le Crab <wiRUzA@ya.ru>
2025-03-01 19:21:06 +0100

    Adapt beyond algorithm for new generator

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ def arithmetic_progression(a, n, d):
     yield from (a + (n - 1) * d for n in rng_generator(n))
 
 def arithmetic_progression_beyond(a, d):
-    yield from (a + n * d for n in rng_generator('inf'))
+    yield from (a + n * d for n in rng_generator())
